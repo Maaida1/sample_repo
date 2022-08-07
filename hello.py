04309fd5d7de153bee246b8503271880c0b3c3c7
Maaida Omar <momar18@terpmail.umd.edu>
2022-08-07 15:56:42 -0400

    More modifications

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 
-"""
 
-A simple function that prints out 'Hello World!'"
-
-"""
 
 def my_function():
-  print("Hello World!")
+    """
+    A simple function that prints out 'Hello World!' "
+    """
+    print("Hello World!")
+  
+
+def new_function():
+    """
+    A simple function that prints out 'Goodbye' "
+    """     
+    print("Goodbye!")
   
 my_function()
+new_function()
