8050bf027a7ecfb5d05cfc4c2d4ebe7a0c640dc2
Maaida Omar <momar18@terpmail.umd.edu>
2022-08-07 15:49:47 -0400

    Even more changes.

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 " A simple function that prints out 'Hello World!'"
 print("Hello World! ")
 print("Making some more changes.")
+print("Printin a function that says Hello.")
 def my_function():
   print("Hello from a function")
   
