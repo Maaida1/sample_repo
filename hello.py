3ad57f76798ce92ee6acf9690ca5fad070d536e4
Maaida Omar <momar18@terpmail.umd.edu>
2022-08-07 15:53:10 -0400

    Finished code.

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
-" A simple function that prints out 'Hello World!'"
-print("Printing a function that says Hello World.")
+
+"""
+
+A simple function that prints out 'Hello World!'"
+
+"""
+
 def my_function():
   print("Hello World!")
   
